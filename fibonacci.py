66176d7bee2fbb4ee18dc3f42a0062797fd72177
NakajimaShinya <43875091+NakajimaShinya@users.noreply.github.com>
2018-10-12 17:33:35 +0900

    10/12 課題

--- fibonacci.py
+++ fibonacci.py
@@ -5,8 +5,12 @@ Created on Fri Oct 12 16:53:02 2018
 @author: Owner
 """
 def fib(n):
-    if n > 1:
-    return fib(n-1) + fib(n-2)
+    if n <= 2:
+        return 1
     else:
-    return 1
-print(fib(4))
+        return fib(n-1) + fib(n-2)
+result = []
+for n in range(1,100):
+    result.append(fib(n))
+print(result)
+    
